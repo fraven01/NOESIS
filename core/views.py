6ca3bf1090d37b682a0b3ce72e1832b12e7028d9
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-06-04 15:09:59 +0200

    Lokalisierung und Authentifizierung

--- core/views.py
+++ core/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
 
+@login_required
 def work(request):
     return render(request, 'work.html')
 
 
+@login_required
 def personal(request):
     return render(request, 'personal.html')
+
+
+@login_required
+def account(request):
+    return render(request, 'account.html')
