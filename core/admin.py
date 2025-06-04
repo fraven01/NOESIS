ce10447d3519565953130a0cf48ddc7403d98998
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-06-04 22:17:10 +0200

    Add TalkDiary feature

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,4 @@ from .models import Recording
 
 @admin.register(Recording)
 class RecordingAdmin(admin.ModelAdmin):
-    list_display = ("user", "bereich", "audio_file", "created_at")
+    list_display = ("user", "bereich", "audio_file", "created_at", "duration")
