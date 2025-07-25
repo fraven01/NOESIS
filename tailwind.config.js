bc76589dfe356268e59ca8b5b1d38333cb0193e5
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-07-25 07:20:50 +0200

    feat: integriere tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./templates/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        'primary-dark': '#1e40af',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
